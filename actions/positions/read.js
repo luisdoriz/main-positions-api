2cb182f8d2b27d427b8da6b6a195879a991553fa
luisdoriz <luis.doriz@udem.edu>
2021-11-17 11:27:07 -0700

    feat[Positions]: add get positons endpoints

--- actions/positions/read.js
+++ actions/positions/read.js
@@ -1,38 +1,77 @@
 const moment = require("moment");
-const { Op,  } = require('sequelize')
+const { Op } = require("sequelize");
 const { sequelize, Position } = require("../../models");
 
 const readPositionsPerson24h = async ({ idPerson, area, from, to }) => {
-    return Position.findAll({
-        where: {
-            idPerson,
-            from: { //only get rows from past 24 hours
-                [Op.gte]: moment(to).subtract(24, 'hours').toDate(),
-            }
-        },
-        order: [
-            ['from', 'ASC'],
-        ],
-        raw: true
-    })
+  return Position.findAll({
+    where: {
+      idPerson,
+      from: {
+        //only get rows from past 24 hours
+        [Op.gte]: moment(to).subtract(24, "hours").toDate(),
+      },
+    },
+    order: [["from", "ASC"]],
+    raw: true,
+  });
 };
 
 const readPersonsPositionsArea = async ({ area, to }) => {
-    return Position.count({
-
-        where: {
-            idArea: area,
-            to: { //only get rows from past 5 minutes. if exist edit their "to" else create new position row
-                [Op.gte]: moment(to).subtract(5, 'minutes').toDate(),
-            }
-        },
-        distinct:true, 
-        col: 'idPerson',        
-        raw: true
-    })
+  return Position.count({
+    where: {
+      idArea: area,
+      to: {
+        //only get rows from past 5 minutes. if exist edit their "to" else create new position row
+        [Op.gte]: moment(to).subtract(5, "minutes").toDate(),
+      },
+    },
+    distinct: true,
+    col: "idPerson",
+    raw: true,
+  });
 };
 
+const getActualPositions = async (idFacility) => {
+  let [positions] = await sequelize.query(
+    `
+    SELECT DISTINCT ON ("Person"."idPerson")
+        "Position"."idPosition" AS "idPosition",
+        "Position"."x" AS "x",
+        "Position"."y" AS "y",
+        "Position"."from" AS "from",
+        "Position"."to" AS "to",
+        "Position"."idArea" AS "idArea",
+        "Position"."isActive" AS "isActive",
+        "Position"."CreationDate" AS "CreationDate",
+        "Person"."idPerson" AS "idPerson",
+        "Person"."name" AS "name",
+        "Person"."firstLastName" AS "firstLastName",
+        "Person"."secondLastName" AS "secondLastName",
+        "Person"."email" AS "email"
+    FROM
+        "Position"
+        LEFT JOIN "Person" ON "Person"."idPerson" = "Position"."idPerson"
+    WHERE 
+       "Person"."idFacility" = :idFacility
+    ORDER BY
+        "Person"."idPerson",
+        "Position"."to" DESC,
+        "Position"."CreationDate" DESC     
+    `,
+    {
+      replacements: { idFacility },
+    }
+  );
+  return positions;
+};
+// ("Position"."CreationDate" >= date_trunc('minute', CAST((CAST(now() AS timestamp) + (INTERVAL '-5 minute')) AS timestamp))
+// AND "Position"."CreationDate" < date_trunc('minute', CAST(now() AS timestamp)))
+// OR("Position"."to" >= date_trunc('minute', CAST((CAST(now() AS timestamp) + (INTERVAL '-5 minute')) AS timestamp))
+// AND "Position"."to" < date_trunc('minute', CAST(now() AS timestamp)))
+// AND 
+
 module.exports = {
-    readPositionsPerson24h,
-    readPersonsPositionsArea
+  readPositionsPerson24h,
+  readPersonsPositionsArea,
+  getActualPositions,
 };
--- controllers/positions.js
+++ controllers/positions.js
@@ -1,48 +1,76 @@
-const Positions = require('../actions/positions');
-const { generateAlerts } = require('./alerts')
-exports.getPositions = async (req, res) => {
-    return
-};
+const Positions = require("../actions/positions");
+const { generateAlerts } = require("./alerts");
 
 exports.postPositions = async (req, res) => {
-    const { x, y, from, to, idArea, idBeacon } = req.body;
-    try {
-        const position = await Positions.createPosition({
-            x, y, from, to, idArea, idBeacon,
-            isActive: 1,
-            CreatedBy: req.user.idUser,
-            UpdatedBy: req.user.idUser
-        })
-        res.status(201).json({ status: 'success', data: position });
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({ status: 'error', error });
-    }
+  const { x, y, from, to, idArea, idBeacon } = req.body;
+  try {
+    const position = await Positions.createPosition({
+      x,
+      y,
+      from,
+      to,
+      idArea,
+      idBeacon,
+      isActive: 1,
+      CreatedBy: req.user.idUser,
+      UpdatedBy: req.user.idUser,
+    });
+    res.status(201).json({ status: "success", data: position });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "error", error });
+  }
 };
+
+exports.getPositions = async (req, res) => {
+  const { idFacility } = req.query;
+  try {
+    const positions = await Positions.getActualPositions(idFacility);
+    res.status(201).json({ status: "success", data: positions });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "error", error });
+  }
+};
+
 exports.putPositions = async (req, res) => {
-    //data from flask. create or update position and check if alert is generated
-    const { positions } = req.body;
-    console.log(positions)
-    try {
-        for (let position of positions) {
-            const { x, y, from, to, area, beacon } = position;
-            const { idBeacon, idPerson, idPrivilegeLevel } = await Positions.upsertPosition({
-                x, y, from, to, area, beacon,
-                isActive: 1,
-                CreatedBy: req.user.idUser,
-                UpdatedBy: req.user.idUser
-            })
-            generateAlerts({ x, y, from, to, area, beacon, idBeacon, idPerson, idPrivilegeLevel })
-        }
-        res.status(200).json({ status: 'success', data: positions.length });
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({ status: 'error', error });
+  //data from flask. create or update position and check if alert is generated
+  const { positions } = req.body;
+  console.log(positions);
+  try {
+    for (let position of positions) {
+      const { x, y, from, to, area, beacon } = position;
+      const { idBeacon, idPerson, idPrivilegeLevel } =
+        await Positions.upsertPosition({
+          x,
+          y,
+          from,
+          to,
+          area,
+          beacon,
+          isActive: 1,
+          CreatedBy: req.user.idUser,
+          UpdatedBy: req.user.idUser,
+        });
+      generateAlerts({
+        x,
+        y,
+        from,
+        to,
+        area,
+        beacon,
+        idBeacon,
+        idPerson,
+        idPrivilegeLevel,
+      });
     }
+    res.status(200).json({ status: "success", data: positions.length });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "error", error });
+  }
 };
 
 exports.deletePositions = async (req, res) => {
-    return
+  return;
 };
-
-
