cab4e25fe0052d269b296623b70a2b5103d9a7fa
Bernardo <bernardocardenas98@gmail.com>
2021-11-26 18:52:58 -0600

    delete users on delete org

--- actions/organizations/delete.js
+++ actions/organizations/delete.js
@@ -1,9 +1,14 @@
-const { Organization } = require('../../models');
+const { Organization, Users } = require('../../models');
 const { deleteFacility, readFacilityByIdOrganization } = require('../facilities')
 
 const deleteOrganization = async ({ idOrganization }) => {
     const facilities = await readFacilityByIdOrganization({ idOrganization })
     await Organization.destroy({
+        where: {
+            idOrganization
+        }
+    });
+    await Users.destroy({
         where: {
             idOrganization
         }
